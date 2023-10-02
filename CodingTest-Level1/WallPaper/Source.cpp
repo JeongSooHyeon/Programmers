35408cee402966d49b9bf30b7e9ff40d8a437df9
SooHyeon Jeong <jsh75721@naver.com>
2023-10-02 23:58:48 +0900

    바탕화면 정리 ing

---
+++ CodingTest-Level1/WallPaper/Source.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+// 빈칸. 파일칸#
+
+vector<int> solution(vector<string> wallpaper) {
+    vector<int> answer;
+
+    int lux=50, luy=50;   // 시작점
+    int rdx=-1, rdy=-1;   // 끝점
+
+    for (int i = 0; i < wallpaper.size(); i++) {
+        for (int j = 0; j < wallpaper[i].size(); j++) {
+            if (wallpaper[i][j] == '#') {   // 파일칸
+                if (lux > j) {  // 가장 작은 시작점
+                    lux = j;
+                }
+                if (luy > i) {
+                    luy = i;
+                }
+                if (rdx < j) {  // 가장 큰 끝점
+                    rdx = j;
+                }
+                if (rdy < i) {
+                    rdy = i;
+                }
+            }
+        }
+    }
+
+    answer.push_back(lux);
+    answer.push_back(luy);
+    answer.push_back(rdx);
+    answer.push_back(rdy);
+
+    return answer;
+}
+
+int main() {
+    vector<string> wallpaper = { ".#...", "..#..", "...#." };
+    //vector<string> wallpaper = { "..........", ".....#....", "......##..", "...##.....", "....#....." };
+    //vector<string> wallpaper = { ".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#....", "....#....." };
+    //vector<string> wallpaper = { "..", "#." };
+
+    solution(wallpaper);
+}
